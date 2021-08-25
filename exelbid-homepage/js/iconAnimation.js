88439d2ec24d62095a207d239671ee781f493330
jamesss <minjun.lee@motiv-i.com>
2021-08-25 14:55:28 +0900

    terms 마무리

---
+++ exelbid-homepage/js/iconAnimation.js
@@ -0,0 +1,14 @@
+$('#navbtn').on('click', function () {
+    $(this).toggleClass('is-active');
+  });
+  
+  function check (status) {
+    for (var x = 0; x < inputs.length; x++){
+      inputs[x].checked = status;
+    }
+    if(status){
+      setTimeout(function() { 
+        check(false);
+      }, 2000);
+    }
+  }
